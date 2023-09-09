3351f6c039200a29eaadd5e69822159fb3d3a748
neeraj@pairoxz <neeraj@pairroxz.com>
2023-09-09 17:59:28 +0530

    updated button

--- js/loading.js
+++ js/loading.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     const baseUrl = "https://neerajchoudhary220.github.io/mycodingno1_blog_files";
-    $('#testBtn').click(function(){
-        const url = baseUrl+files.pages.page1;
-        console.log(url);
-        $("#loadData").load(url);
-    });
+    const url = baseUrl+files.pages.page1;
+    $("#loadData").load(url);
+
 })
